46a8adea862c2dd1f00624580df9f0dcac77a32a
Tim Kleier <timkleier@gmail.com>
2017-04-19 13:42:12 -0600

    Updated custom list cell for full-cell tappability

--- EAB/EAB/CustomListCell.cs
+++ EAB/EAB/CustomListCell.cs
@@ -1,50 +1,78 @@
-﻿using Xamarin.Forms;
-using XLabs.Enums;
-using XLabs.Forms.Controls;
+﻿using System;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using EAB.Helpers;
 
 namespace EAB
 {
     public class CustomListCell : ViewCell
     {
+        public static BindableProperty CommandProperty = BindableProperty.Create<CustomListCell, ICommand>(x => x.Command, null);
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public Task Navigation { get; private set; }
 
         public CustomListCell()
         {
+
+            var helpers = new ViewHelpers();
             // Instantiate Views
-            var image = new Image();
+            var image = new Image { Aspect = Aspect.AspectFill };
             var title = new Label();
             var description = new Label();
-            var arrow = new ImageButton() { Image = "arrow.png", BackgroundColor = Color.Transparent };
-            var cellLayout = new StackLayout() { BackgroundColor = Color.FromHex("88cececc"), Padding = new Thickness(20, 10, 20, 10), Margin = new Thickness(0, 10, 0, 10) };
-            Grid innerLayout = new Grid { ColumnSpacing = 20 };
+            var arrow = new Image { Source = "arrow.png", BackgroundColor = Color.Transparent };
+            Grid cellLayout = new Grid {
+                ColumnSpacing = 15,
+                BackgroundColor = Color.FromHex("88cececc"),
+                Padding = new Thickness(15, 5, 15, 5),
+                Margin = new Thickness(0, 10, 0, 10)
+            };
+
+            // Add gesture recognizer for navigation
+            var gestureRecognizer = new TapGestureRecognizer();
+            gestureRecognizer.Tapped += (s, e) => {
+                if (Command != null && Command.CanExecute(null))
+                {
+                    Command.Execute(null);
+                }
+            };
 
             // Set Bindings
             image.SetBinding(Image.SourceProperty, new Binding("Thumbnail"));
             title.SetBinding(Label.TextProperty, new Binding("Text"));
             description.SetBinding(Label.TextProperty, new Binding("Detail"));
-            arrow.SetBinding(ImageButton.CommandProperty, new Binding("NavigationCommand"));
+            SetBinding(CommandProperty, new Binding("NavigationCommand"));
+            cellLayout.GestureRecognizers.Add(gestureRecognizer);          
 
             // Set properties for desired design
-            innerLayout.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            innerLayout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(3, GridUnitType.Star) });
-            innerLayout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(10, GridUnitType.Star) });
-            innerLayout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
-            image.HorizontalOptions = LayoutOptions.End;
+            cellLayout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(80) });
+            cellLayout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            cellLayout.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(40) });
+            cellLayout.RowDefinitions.Add(new RowDefinition { Height = new GridLength(80) });
             title.FontSize = 18;
             description.FontSize = 14;
 
             // Join title and description
-            StackLayout titleAndDescription = new StackLayout { VerticalOptions = LayoutOptions.Center };
+            StackLayout titleAndDescription = new StackLayout {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Start
+            };
             titleAndDescription.Children.Add(title);
             titleAndDescription.Children.Add(description);
 
             // Add views to hierarchy
-            innerLayout.Children.Add(image, 0, 0);
-            innerLayout.Children.Add(titleAndDescription, 1, 0);
-            innerLayout.Children.Add(arrow, 2, 0);
-
-            cellLayout.Children.Add(innerLayout);
+            cellLayout.Children.Add(image, 0, 0);
+            cellLayout.Children.Add(titleAndDescription, 1, 0);
+            cellLayout.Children.Add(arrow, 2, 0);
 
             View = cellLayout;
         }
+
     }
 }
